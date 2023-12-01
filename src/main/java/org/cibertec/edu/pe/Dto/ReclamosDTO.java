4780c766771a5994441b5ff001576a162454d6c6
Rodolfo <rodolfojaralopez19@gmail.com>
2023-11-30 21:00:26 -0500

    first commit add

---
+++ src/main/java/org/cibertec/edu/pe/Dto/ReclamosDTO.java
@@ -0,0 +1,59 @@
+package org.cibertec.edu.pe.Dto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+
+public class ReclamosDTO {
+	@JsonInclude(JsonInclude.Include.NON_NULL) 
+	public List<ReclamosDTO> getReclamos() {
+		return reclamos;
+	}
+	public void setReclamos(List<ReclamosDTO> reclamos) {
+		this.reclamos = reclamos;
+	}
+
+	private List<ReclamosDTO> reclamos;
+	 private Long id;
+	    private String nombre;
+	    private String reclamo;
+
+	    // Constructor, getters y setters
+	    public ReclamosDTO(List<ReclamosDTO> reclamos) {
+	        this.reclamos = reclamos;
+	    }
+	    public ReclamosDTO(Long id, String nombre, String reclamo) {
+	        this.id = id;
+	        this.nombre = nombre;
+	        this.reclamo = reclamo;
+	    }
+
+	    public ReclamosDTO() {
+			super();
+		}
+
+		public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public String getReclamo() {
+	        return reclamo;
+	    }
+
+	    public void setReclamo(String reclamo) {
+	        this.reclamo = reclamo;
+	    }
+    
+    
+}
--- src/main/java/org/cibertec/edu/pe/controller/ReclamosController.java
+++ src/main/java/org/cibertec/edu/pe/controller/ReclamosController.java
@@ -1,9 +1,14 @@
 package org.cibertec.edu.pe.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.cibertec.edu.pe.Dto.ReclamosDTO;
 import org.cibertec.edu.pe.model.Reclamos;
 import org.cibertec.edu.pe.repository.ReclamoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +26,25 @@ public class ReclamosController {
 	public void reclamar(@RequestBody Reclamos r) {
 		reclamoRepository.save(r);
 	}
+
+	
 	@GetMapping("/listar")
-    public List<Reclamos> ObtenerTodo() {
-        return reclamoRepository.findAll();
+    public ResponseEntity<ReclamosDTO> ObtenerTodo() {
+        List<Reclamos> reclamos = reclamoRepository.findAll();
+
+        if (reclamos.isEmpty()) {
+            // Si no hay reclamos, devolver una respuesta HTTP 204 No Content
+            return ResponseEntity.noContent().build();
+        } else {
+            // Si hay reclamos, mapear la lista de entidades a la lista de DTOs y devolver una respuesta HTTP 200 OK
+            List<ReclamosDTO> reclamosDTOs = reclamos.stream()
+                    .map(reclamo -> new ReclamosDTO(reclamo.getId(), reclamo.getNombre(), reclamo.getReclamo()))
+                    .collect(Collectors.toList());
+
+            ReclamosDTO reclamosResponse = new ReclamosDTO(reclamosDTOs);
+            return ResponseEntity.ok(reclamosResponse);
+        }
     }
+	
+
 }
